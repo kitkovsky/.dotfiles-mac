e0cac7752b1aa4383ffbf5b241ec67d4f6d70e90
kitkovsky <okitkowski114@gmail.com>
2023-10-16 14:02:41 +0200

    update telescope branch & clean up config

--- nvim/.config/nvim/lua/kitkovsky/packer.lua
+++ nvim/.config/nvim/lua/kitkovsky/packer.lua
@@ -8,7 +8,7 @@ return require("packer").startup(function(use)
 
 	use({
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.0",
+		branch = "0.1.x",
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
 
