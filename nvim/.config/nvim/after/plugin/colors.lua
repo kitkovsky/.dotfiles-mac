cd25217d4b318028264f10f799dcfead3db7c36c
kitkovsky <okitkowski114@gmail.com>
2025-12-07 18:26:29 +0100

    general: remove rose-pine

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,31 +1,9 @@
-local GRUVBOX_BABY = "gruvbox"
-local ROSE_PINE = "rose-pine"
-local current_colorscheme = GRUVBOX_BABY
-
 vim.o.background = "dark"
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
-if current_colorscheme == ROSE_PINE then
-	require("rose-pine").setup({
-		variant = "moon",
-		disable_background = true,
+vim.cmd("colorscheme gruvbox")
 
-		highlight_groups = {
-			TelescopeBorder = { fg = "highlight_high", bg = "none" },
-			TelescopeNormal = { bg = "none" },
-			TelescopePromptNormal = { bg = "base" },
-			TelescopeResultsNormal = { fg = "subtle", bg = "none" },
-			TelescopeSelection = { fg = "text", bg = "base" },
-			TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
-		},
-	})
-end
-
-vim.cmd("colorscheme " .. current_colorscheme)
-
-if current_colorscheme == GRUVBOX_BABY then
-	vim.cmd("highlight SignColumn guibg=NONE")
-	vim.cmd("highlight Normal guibg=none")
-	vim.cmd("highlight WinBar guibg=none")
-end
+vim.cmd("highlight SignColumn guibg=NONE")
+vim.cmd("highlight Normal guibg=none")
+vim.cmd("highlight WinBar guibg=none")
--- nvim/.config/nvim/lua/kitkovsky/lazy.lua
+++ nvim/.config/nvim/lua/kitkovsky/lazy.lua
@@ -15,7 +15,6 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	"ellisonleao/gruvbox.nvim",
-	-- { "rose-pine/neovim", name = "rose-pine" },
 
 	{
 		"nvim-telescope/telescope.nvim",
