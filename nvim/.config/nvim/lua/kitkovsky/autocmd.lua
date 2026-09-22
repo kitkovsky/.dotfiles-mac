1b9ce5e650423b697722615bc142fcb62942a825
kitkovsky <okitkowski114@gmail.com>
2026-09-22 11:04:49 +0200

    flutter: add app refresh for tmux windows 4 and 5

--- nvim/.config/nvim/lua/kitkovsky/autocmd.lua
+++ nvim/.config/nvim/lua/kitkovsky/autocmd.lua
@@ -18,11 +18,9 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 		-- 3) send the 'r' key to that tmux window
 		vim.fn.system({ "tmux", "send-keys", "-t", target, "r" })
 
-		-- 1) get the current tmux session name (strip trailing newline)
-		local session = vim.fn.trim(vim.fn.system('tmux display-message -p "#S"'))
-		-- 2) compose target as session:4 (the fourth window)
-		local target = session .. ":4"
-		-- 3) send the 'r' key to that tmux window
+		target = session .. ":4"
+		vim.fn.system({ "tmux", "send-keys", "-t", target, "r" })
+		target = session .. ":5"
 		vim.fn.system({ "tmux", "send-keys", "-t", target, "r" })
 	end,
 })
